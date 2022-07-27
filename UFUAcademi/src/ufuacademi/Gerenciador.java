c42601d2c2d452db05ef8f96d1ee23032c637627
Victor Silva <victorhfsilva@protonmail.com>
2022-07-26 21:29:47 -0300

    Trabalho MTP 26/07/2022

---
+++ UFUAcademi/src/ufuacademi/Gerenciador.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ufuacademi;
+
+/**
+ *
+ * @author victor
+ */
+public interface Gerenciador {
+    public void novo();
+    public void editar();
+    public void deletar();
+    public void importar();
+    public void exportar();
+}
